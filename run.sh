9c4e9a2120b4dd78f42a93d38b8abad5bdc5cf47
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-07-02 00:01:40 +0900

    Edit run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,15 @@
 
 export DOCKER_BUILDKIT=1
 
-docker build -t riscv:latest -f Dockerfile .
-docker run -v "$(pwd):/work" -w "/work" --rm -it riscv:latest
+docker build \
+  -t riscv:latest \
+  -f Dockerfile \
+  .
+docker run \
+  --mount "type=bind,src=/etc/passwd,dst=/etc/passwd,readonly" \
+  --mount "type=bind,src=/etc/group,dst=/etc/group,readonly" \
+  -v "$(pwd):/work" \
+  -w "/work" \
+  --rm \
+  -it \
+  riscv:latest
